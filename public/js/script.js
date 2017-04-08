484ce37632f3b815bfa1a9abe3207677625e022f
David <David@Davids-MacBook-Pro.local>
2017-04-08 17:40:18 -0600

    Got Jquery to work for us

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,20 @@
 $( document ).ready(function() {
-  $('.task').click(function() {
-      $(this).fadeOut( "slow", function() {
-        if($(this).find("input").is(':checked')){
-          $(this).appendTo(".finished-list").addClass("finished-task");
-        }else{
-          $(this).appendTo(".todo-list").removeClass("finished-task");
-        }
-        $(this).fadeIn("slow");
-      });
+  $("button").click(function(){
+    console.log($('#taskInput').val() );
+    $(".todo-list").append('<li class="task"><input type = "checkbox"/>' + $('#taskInput').val()+'</li>');
   });
+
+  $(document).on("click",".task",function(){
+    $(this).fadeOut( "slow", function() {
+      if($(this).find("input").is(':checked')){
+        $(this).appendTo(".finished-list").addClass("finished-task");
+      }else{
+        $(this).appendTo(".todo-list").removeClass("finished-task");
+      }
+
+      $(this).fadeIn("slow");
+    });
+  });
+  
   $(".finished-task input").prop( "disabled", true ); //Disable
 });
